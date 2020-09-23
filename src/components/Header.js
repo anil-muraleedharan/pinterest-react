5ca4beeed064ed24e06bb3b84b06f74cd14ccd96
anil-muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-09-24 03:32:04 +0530

    Added a add icon in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../images/logo.png';
+import add from '../images/add.png';
 
 const Header = (props) => (
   <div className='header'>
     <Link to='/'>
       <img src={logo} className='logo' />
+    </Link>
+    <Link to='/newPin'>
+      <img src={add} className='add-icon' />
     </Link>
   </div>
 );
