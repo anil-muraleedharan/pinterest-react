eb01213d63ab183be543df01a8aea8bbbad03dff
anil-muraleedharan <56071561+anil-muraleedharan@users.noreply.github.com>
2020-09-23 19:57:01 +0530

    Styled Pin Component

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -4,10 +4,12 @@ const Pin = ({ pinData }) => {
   const { id, imageSrc, title, description } = pinData;
   return (
     <div className='pin'>
-      <img src={imageSrc} alt={`image_${id}`} />
-      <div>
-        <h3>{title}</h3>
-        <p>{description}</p>
+      <div className='pin-image-container'>
+        <img src={imageSrc} alt={`image_${id}`} className='pin-image' />
+      </div>
+      <div className='pin-details'>
+        <h3 className='pin-title'>{title}</h3>
+        <p className='pin-description'>{description}</p>
       </div>
     </div>
   );
