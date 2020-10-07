eab73b928c7b932e0acdc73b934d0b2604639f6c
Anil Muraleedharan <anilmuraleedharan@Anils-MacBook-Pro.local>
2020-10-07 22:10:58 +0530

    Made Cards component into a styled component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import Card from './Card';
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import Card from './Card';
+
+const CardsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const PinLink = styled(Link)`
+  display: block;
+  width: 230px;
+  overflow: hidden;
+  border-radius: 12px;
+  margin: 20px;
+  box-shadow: 0px 0px 4px #ccc;
+  &:hover {
+    box-shadow: 0px 0px 12px #aaa;
+  }
+`;
 
 const Cards = (props) => {
   const cards = props.cardsData.map((cardData, index) => (
-    <Link to={`/pin/${cardData.id}`} className='pinLink' key={index + 1}>
+    <PinLink to={`/pin/${cardData.id}`} key={index + 1}>
       <Card key={cardData.id} cardData={cardData} />
-    </Link>
+    </PinLink>
   ));
-  return <div className='cards'>{cards}</div>;
+  return <CardsContainer>{cards}</CardsContainer>;
 };
 
 export default Cards;
