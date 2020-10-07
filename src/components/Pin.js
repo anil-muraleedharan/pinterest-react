446409bb7f559f8f138960cbfa894b2c139e0eb6
Anil Muraleedharan <anilmuraleedharan@Anils-MacBook-Pro.local>
2020-10-07 16:05:28 +0530

    Added a loading state and message

--- src/components/Pin.js
+++ src/components/Pin.js
@@ -4,22 +4,17 @@ import Like from './Like';
 
 const Pin = (props) => {
   const { id } = useParams();
+  const [isLoaded, setIsLoaded] = useState(false);
   const [pinDetails, setPinDetails] = useState({});
   const [likesCount, setLikesCount] = useState(0);
-  const {
-    imageSrc,
-    title,
-    description,
-    name,
-    avatar_url,
-    createdAt,
-    createdBy,
-  } = pinDetails;
 
   useEffect(() => {
     fetch(`/api/getPin/${id}`)
       .then((res) => res.json())
-      .then(setPinDetails);
+      .then((details) => {
+        setPinDetails(details);
+        setIsLoaded(true);
+      });
   }, [id, likesCount]);
 
   useEffect(() => {
@@ -34,32 +29,47 @@ const Pin = (props) => {
     })
       .then((res) => res.json())
       .then(({ likes }) => setLikesCount(likes));
-
-  return (
-    <div className='pin'>
-      <div className='pin-image-container'>
-        <img src={imageSrc} alt={`image_${id}`} className='pin-image' />
-      </div>
-      <div className='pin-details'>
-        <h3 className='pin-title'>{title}</h3>
-        <p className='pin-description'>{description}</p>
-        <p className='created-time'>
-          Created on {new Date(createdAt).toLocaleString()}
-        </p>
-        <div className='pin-add-details'>
-          <Link to={`/user/${createdBy}`} style={{ textDecoration: 'none' }}>
-            <div className='pin-creator'>
-              <img
-                src={avatar_url}
-                className='creator-avatar'
-                alt='creator-avatar'
-              />
-              <p className='creator-name'>{name}</p>
-            </div>
-          </Link>
-          <Like likes={likesCount} addLike={addLike} />
+  if (isLoaded) {
+    const {
+      imageSrc,
+      title,
+      description,
+      name,
+      avatar_url,
+      createdAt,
+      createdBy,
+    } = pinDetails;
+    return (
+      <div className='pin'>
+        <div className='pin-image-container'>
+          <img src={`/${imageSrc}`} alt={`image_${id}`} className='pin-image' />
+        </div>
+        <div className='pin-details'>
+          <h3 className='pin-title'>{title}</h3>
+          <p className='pin-description'>{description}</p>
+          <p className='created-time'>
+            Created on {new Date(createdAt).toLocaleString()}
+          </p>
+          <div className='pin-add-details'>
+            <Link to={`/user/${createdBy}`} style={{ textDecoration: 'none' }}>
+              <div className='pin-creator'>
+                <img
+                  src={avatar_url}
+                  className='creator-avatar'
+                  alt='creator-avatar'
+                />
+                <p className='creator-name'>{name}</p>
+              </div>
+            </Link>
+            <Like likes={likesCount} addLike={addLike} />
+          </div>
         </div>
       </div>
+    );
+  }
+  return (
+    <div className='pin'>
+      <p>Loading...</p>
     </div>
   );
 };
