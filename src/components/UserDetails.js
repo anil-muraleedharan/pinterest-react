8c44f5e8d1d31824e8a3c022841d91eb92496678
Anil Muraleedharan <anilmuraleedharan@Anils-MacBook-Pro.local>
2020-10-08 22:45:44 +0530

    Modified UserDetails with styled components

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 20px 50px 30px 0;
+`;
+
+const Avatar = styled.img`
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
+`;
+
+const Name = styled.p`
+  color: black;
+  font-size: 24px;
+  margin: 0px;
+  opacity: 0.8;
+  font-family: 'RobotoCondensed';
+  font-weight: bolder;
+`;
 
 const UserDetails = ({ id, name, avatar }) => (
   <Link to={`/user/${id}`} style={{ textDecoration: 'none' }}>
-    <div className='pin-creator'>
-      <img src={avatar} className='creator-avatar' alt='creator-avatar' />
-      <p className='creator-name'>{name}</p>
-    </div>
+    <Container>
+      <Avatar src={avatar} alt='creator-avatar' />
+      <Name className='creator-name'>{name}</Name>
+    </Container>
   </Link>
 );
 
