cad2377784c4618b89588f3131b0f3e2fb975763
Anil Muraleedharan <anilmuraleedharan@Anils-MacBook-Pro.local>
2020-10-08 15:47:18 +0530

    Modified UserProfile with styled component

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import Dashboard from './Dashboard';
+
+const Avatar = styled.img`
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+`;
+
+const Username = styled.p`
+  font-size: 40px;
+  font-family: 'RobotoCondensedBold';
+  margin: 15px;
+`;
+
+const UserCard = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 10px 0 25px 0;
+`;
 
 const UserProfile = (props) => {
   const { id } = useParams();
@@ -14,14 +33,10 @@ const UserProfile = (props) => {
 
   return (
     <div>
-      <div className='profile-header'>
-        <img
-          src={userData.avatar_url}
-          className='profile-avatar'
-          alt='avatar'
-        />
-        <p className='profile-username'>{userData.name}</p>
-      </div>
+      <UserCard>
+        <Avatar src={userData.avatar_url} alt='avatar' />
+        <Username>{userData.name}</Username>
+      </UserCard>
       <Dashboard dataURL={`/api/getUserPins/${id}`} isHomePage={false} />
     </div>
   );
