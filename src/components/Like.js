46682f88d3791830c1e55ce608731207acc8b10b
Anil Muraleedharan <anilmuraleedharan@Anils-MacBook-Pro.local>
2020-10-08 15:54:04 +0530

    Modified Like component with styled component

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import styled from 'styled-components';
 import likeIcon from '../images/like.png';
+
+const LikeIcon = styled.img`
+  width: 25px;
+  height: 25px;
+  opacity: 0.8;
+`;
+
+const LikesCount = styled.p`
+  font-family: 'RobotoCondensed';
+  font-weight: bold;
+  font-size: 19px;
+  margin: 4px 12px;
+`;
+
+const LikesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 20px 50px 30px 0;
+  cursor: pointer;
+`;
 
 const Like = (props) => {
   const message = `${props.likes} ${props.likes === 1 ? 'Like' : 'Likes'}`;
   return (
-    <div className='likes-container' onClick={() => props.addLike()}>
-      <img src={likeIcon} className='like-icon' alt='like' />
-      <p className='likes-count'>{message}</p>
-    </div>
+    <LikesContainer onClick={() => props.addLike()}>
+      <LikeIcon src={likeIcon} alt='like' />
+      <LikesCount>{message}</LikesCount>
+    </LikesContainer>
   );
 };
 
